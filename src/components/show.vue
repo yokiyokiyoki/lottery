94c1aa9dde29999c08f79ec97606d92b2c2de7a1
yokiyokiyoki <419703726@qq.com>
2018-01-23 11:42:05 +0800

    show页面展示

--- src/components/show.vue
+++ src/components/show.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="show">
-      {{allCount}}
+      <div v-for='(value,key,index) in allCount' :key='index'>
+          <h1>{{transform(key)}}等奖</h1>
+          <div class="box">
+            <span v-for='i in value' :key='i'>{{i}}</span>
+          </div>
+      </div>
   </div>
 </template>
 <script>
@@ -10,8 +15,34 @@ export default {
       allCount: {}
     };
   },
+  methods: {
+    transform(val) {
+      if (val == 0) {
+        return "特";
+      } else if (val == 1) {
+        return "一";
+      } else if (val == 2) {
+        return "二";
+      } else if (val == 3) {
+        return "三";
+      } else {
+        return "四";
+      }
+    }
+  },
   mounted() {
     this.allCount = JSON.parse(localStorage.getItem("allLocCount"));
   }
 };
 </script>
+<style lang="less" scoped>
+.box {
+  height: 400px;
+  width: 400px;
+  span {
+    height: 40px;
+    width: 40px;
+    display: inline-block;
+  }
+}
+</style>
