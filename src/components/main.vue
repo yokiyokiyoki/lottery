da370674b0004db74d0b2e202e2a956a546ce1bb
yokiyokiyoki <419703726@qq.com>
2018-01-24 20:12:44 +0800

    提交页面

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="main">
-    <img src="~@/assets/bg.jpg" class="bg">
+    <div class="content">
+      <img src="~@/assets/logo.png" class="logo">
+      <div class="box">
+        <div class="box-header">
+          <h1>{{rankName}}等奖中奖名单</h1>
+        </div>
+        <div class="box-content">
+          1
+        </div>
+        <div class="box-footer">
+          <el-select v-model="selected" placeholder="请选择">
+            <el-option
+              v-for="item in selectedList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+          <div class="button">
+            <el-button type="primary">抽奖</el-button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,21 +33,32 @@ export default {
   components: {
     animatedInteger
   },
-  mounted() {}
-};
-function initCls(val) {
-  if (val == 0) {
-    this.numClass = "num-1";
-  } else if (val == 1) {
-    this.numClass = "num-3";
-  } else if (val == 2) {
-    this.numClass = "num-5";
-  } else if (val == 3) {
-    this.numClass = "num-20";
-  } else {
-    this.numClass = "num-30";
+  mounted() {},
+  data() {
+    return {
+      rankName: "三",
+      selected: 3,
+      selectedList: [
+        {
+          label: "三等奖",
+          value: 3
+        },
+        {
+          label: "二等奖",
+          value: 2
+        },
+        {
+          label: "一等奖",
+          value: 1
+        },
+        {
+          label: "特等奖",
+          value: 0
+        }
+      ]
+    };
   }
-}
+};
 function randomNum(minNum, maxNum) {
   switch (arguments.length) {
     case 1:
@@ -48,8 +82,50 @@ function randomNum(minNum, maxNum) {
   display: flex;
   justify-content: center;
   align-items: center;
-  .bg {
+  .content {
     width: 100%;
+    height: 100%;
+    background: url("~@/assets/bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .logo {
+      width: 500px;
+      position: absolute;
+      top: -80px;
+    }
+    .box {
+      color: white;
+      position: absolute;
+      top: 100px;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      &-header {
+        // color: #4cb2a6;
+        color: #3a8ee6;
+      }
+      &-content {
+      }
+      &-footer {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 50px;
+        display: flex;
+        justify-content: center;
+        .button {
+          display: inline-block;
+          margin-left: 20px;
+          .el-button--primary {
+            width: 100px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
